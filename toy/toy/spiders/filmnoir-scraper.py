f33b962974a00a6e58b753f1700a5aeaf7a60ad8
patrickZWY <154947644+patrickZWY@users.noreply.github.com>
2024-01-08 15:31:52 -0500

    fix filmnoir-scraper

--- toy/toy/spiders/filmnoir-scraper.py
+++ toy/toy/spiders/filmnoir-scraper.py
@@ -1,135 +1,29 @@
-"""
 import scrapy
-import schedule
-import time
-from scrapy import cmdline
 
-# spider
-class FilmNoirSpider(scrapy.Spider):
-    name = 'film_noir'
-    # starting url
-    start_urls = ["https://en.wikipedia.org/wiki/Category:Film_noir"]
+class wikiSpider(scrapy.Spider):
+    name = 'wikiSpider'
+    start_urls = ['https://en.wikipedia.org/wiki/List_of_film_noir_titles']
 
-    # user agent, download delay, concurrent requests
-    custom_settings = {
-        'USER AGENT': 'Mozilla/5.0 (Windows NT 10.0;Win64) \
+    custom_settings = {'USER AGENT': 'Mozilla/5.0 (Windows NT 10.0;Win64) \
         AppleWebkit/537.36 (KHTML, like Gecko) \
         Chrome/89.0.4389.82 Safari/537.36',
         'DOWNLOAD_DELAY' : 1,
         'CONCURRENT_REQUESTS' : 1,
         'RETRY_TIMES' : 3,
         'RETRY_HTTP_CODES' : [500, 503, 504, 400, 403, 404, 408],
+        'FEED_FORMAT': 'csv',
+        'FEED_URI': 'filmNoir_1940.csv',
         'DOWNLOADER_MIDDLEWARES' : {
         'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
         'scrapy.downloadermiddlewares.retry.RetryMiddleware': 90,
         'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 110,
-    }
+        }    
     }
 
     def parse(self, response):
-        films = response.css("div#mw-pages div.mw-category div.mw-category-group ul li")
-        for film in films:
-            title = film.css('a::text').get()
-            url = film.css('a::attr(href)').get()
-            if url:
-                url = response.urljoin(url)
-                yield scrapy.Request(url, callback=self.parse_film, meta={'title': title})
-
-    def parse_film(self, response):
-        title = response.meta['title']
-        year = response.css('table.infobox th:contains("Release date") + td::text').get()
-
-        if year:
-            year = ''.join(filter(str.isdigit, year[:4]))
-        else:
-            year = 'Unknown'
-
-        yield {
-            'title': title,
-            'year': year
-        }
-
-
-# run spider
-def crawl_filmnoir():
-    cmdline.execute("scrapy crawl film_noir".split())
-
-schedule.every(1).minute.do(crawl_filmnoir)
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
-"""
-import scrapy
-import asyncio
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-
-# spider
-class FilmNoirSpider(scrapy.Spider):
-    name = 'film_noir'
-    # starting url
-    start_urls = ["https://en.wikipedia.org/wiki/Category:Film_noir"]
-
-    # user agent, download delay, concurrent requests
-    custom_settings = {
-        'USER AGENT': 'Mozilla/5.0 (Windows NT 10.0;Win64) \
-        AppleWebkit/537.36 (KHTML, like Gecko) \
-        Chrome/89.0.4389.82 Safari/537.36',
-        'DOWNLOAD_DELAY' : 1,
-        'CONCURRENT_REQUESTS' : 1,
-        'RETRY_TIMES' : 3,
-        'RETRY_HTTP_CODES' : [500, 503, 504, 400, 403, 404, 408],
-        'DOWNLOADER_MIDDLEWARES' : {
-        'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
-        'scrapy.downloadermiddlewares.retry.RetryMiddleware': 90,
-        'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 110,
-    }
-    }
-
-    async def parse(self, response):
-        films = response.css("div#mw-pages div.mw-category div.mw-category-group ul li")
-        for film in films:
-            title = film.css('a::text').get()
-            url = film.css('a::attr(href)').get()
-            if url:
-                url = response.urljoin(url)
-                yield scrapy.Request(url, callback=self.parse_film, meta={'title': title})
-
-    async def parse_film(self, response):
-        title = response.meta['title']
-        year_selectors = [
-            'table.infobox th:contains("Release date") + td::text',
-            'table.infobox th:contains("Released") + td::text',
-            'table.infobox th:contains("Release") + td::text'
-        ]
-        year = None
-        for selector in year_selectors:
-            year = response.css(selector).get()
-            if year:
-                break
-        if year:
-            year = ''.join(filter(str.isdigit, year[:4]))
-        else:
-            year = 'Unknown'
-        
-        if year.isdigit() and 1940 <= int(year) <= 1949:
+        # the last thing in response is the one you want to iterate over, in this case it is li not ul
+        # xpath to choose something relative 
+        for movie in response.xpath('//style[@data-mw-deduplicate="TemplateStyles:r1184024115"]/following-sibling::div[1]//ul/li'):
             yield {
-                'title': title,
-                'year': year
+                'title': movie.css('i a::text').get()
             }
-
-
-# run spider
-async def crawl():
-    process = CrawlerProcess(get_project_settings())
-    process.crawl(FilmNoirSpider)
-    await process.start()
-
-async def schedule_crawl():
-    while True:
-        await crawl()
-        await asyncio.sleep(60)
-
-if __name__ == "__main__":
-    asyncio.run(schedule_crawl())
